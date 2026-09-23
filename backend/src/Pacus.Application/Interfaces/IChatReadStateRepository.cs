1189f138522c12141882af4b62e9957ab827e24d
taukkunen1 <68305908+taukkunen1@users.noreply.github.com>
2026-09-23 10:37:31 -0300

    refactor(chat): persistir cursor de leitura

--- backend/src/Pacus.Application/Interfaces/IChatReadStateRepository.cs
+++ backend/src/Pacus.Application/Interfaces/IChatReadStateRepository.cs
@@ -6,7 +6,7 @@ namespace Pacus.Application.Interfaces;
 public interface IChatReadStateRepository
 {
     Task<ChatReadState?> GetAsync(ObjectId familyId, ObjectId userId);
-    Task UpsertAsync(ObjectId familyId, ObjectId userId, DateTime lastReadAt);
+    Task UpsertAsync(ObjectId familyId, ObjectId userId, ObjectId lastReadMessageId, DateTime lastReadAt);
     Task<List<ChatReadState>> GetAllByFamilyAsync(ObjectId familyId);
     Task DeleteAllByFamilyAsync(ObjectId familyId);
 }
